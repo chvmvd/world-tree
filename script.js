f83f9447782f79c1f5de6f19cd8913f7ccc7785b
chvmvd <28106371-chvmvd@users.noreply.replit.com>
2024-01-15 09:25:51 +0000

    ホーム画面のデザインを改善

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
       const newFile = {
         id: self.crypto.randomUUID(),
         title: fileName.trim(),
+        createdAt: new Date().toLocaleString(),
+        updatedAt: new Date().toLocaleString(),
         scenes: [
           {
             id: self.crypto.randomUUID(),
@@ -35,16 +37,45 @@ document.addEventListener("DOMContentLoaded", function() {
   // 既存のファイルをリストアップ
   function updateFileList() {
     fileList.innerHTML = "";
-    const novels = JSON.parse(localStorage.getItem("novels")) || [];
+    fileList.className = "d-flex flex-wrap";
+    let novels = JSON.parse(localStorage.getItem("novels")) || [];
+    novels = novels.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
     novels.forEach((novel) => {
-      const fileElement = document.createElement("div");
-      fileElement.textContent = novel.title || "無題の小説";
-      fileElement.style.cursor = "pointer";
-      localStorage.setItem("novelId", novel.id);
-      fileElement.addEventListener("click", function() {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.style.width = "18rem";
+      card.style.margin = "5px";
+
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+
+      const cardTitle = document.createElement("h5");
+      cardTitle.className = "card-title";
+      cardTitle.textContent = novel.title || "無題の小説";
+
+      const cardText = document.createElement("p");
+      cardText.className = "card-text";
+      cardText.innerText = `作成日時: ${novel.createdAt} \n 最終編集時点: ${novel.updatedAt}`;
+
+      const editButton = document.createElement("button");
+      editButton.className = "btn btn-success ml-auto";
+      editButton.textContent = "編集";
+      editButton.addEventListener("click", function(event) {
+        event.stopPropagation(); // カードのクリックイベントを止める
+        localStorage.setItem("novelId", novel.id);
         window.location.href = `./edit`;
       });
-      fileList.appendChild(fileElement);
+
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardText);
+      cardBody.appendChild(editButton);
+      card.appendChild(cardBody);
+
+      card.addEventListener("click", function() {
+        localStorage.setItem("novelId", novel.id);
+        window.location.href = `./edit`;
+      });
+      fileList.appendChild(card);
     });
   }
 
